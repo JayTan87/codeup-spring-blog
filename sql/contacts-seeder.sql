814f670bfce5b777580a8ba977cbc64b969c0a13
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-09-18 12:01:33 -0500

    update exercise file

---
+++ sql/contacts-seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_blog_db;
+
+INSERT INTO contacts (email, name) VALUES
+                                       ('jeremy@email.com', 'Jeremy'),
+                                       ('joe@email.com', 'Joe'),
+                                       ('jill@email.com', 'Jill'),
+                                       ('bill@email.com', 'Bill'),
+                                       ('steve@email.com', 'Steve');
--- src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
@@ -1,10 +1,12 @@
 package edu.codeup.codeupspringblog.controllers;
 
+import edu.codeup.codeupspringblog.models.Item;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/hello")
@@ -21,4 +23,28 @@ public class HelloController {
     public String helloName(@PathVariable String name) {
         return String.format("Hello, %s!", name);
     }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(Model model) {
+        List<Item> shoppingCart = new ArrayList<>();
+        shoppingCart.add(new Item("screwdriver"));
+        shoppingCart.add(new Item("hammer"));
+        shoppingCart.add(new Item("drill"));
+        model.addAttribute("shoppingCart", shoppingCart);
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
+
 }
